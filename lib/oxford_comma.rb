508e81a3e5c069c3a17f30e114e3b9fd2cfb013a
Alexia Adediran <alexiaadediran30@gmail.com>
2020-02-20 02:21:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -11,3 +11,15 @@
 #   array[0...-1].join(", ") <<", and #{array[-1]}"
 #   end
 # end
+def oxford_comma(array)
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    new_last_array_item = "and #{array[-1]}"
+    array.pop
+    array.push(new_last_array_item)
+    return array.join(", ")
+  end
+end
